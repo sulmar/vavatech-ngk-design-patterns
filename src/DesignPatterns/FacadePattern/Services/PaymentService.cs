d3fb8dab7373b41762637f12de99f4094de2f5e4
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-07-15 17:45:13 +0200

    Refaktoring

--- src/DesignPatterns/FacadePattern/Services/PaymentService.cs
+++ src/DesignPatterns/FacadePattern/Services/PaymentService.cs
@@ -3,17 +3,6 @@
 namespace FacadePattern.Services
 {
 
-    public class TicketCalculator
-    {
-        private readonly decimal amount = 1.99m;
-
-        public decimal Calculate(RailwayConnection railwayConnection, byte numberOfPlaces)
-        {
-            return railwayConnection.Distance * amount * numberOfPlaces;
-        }
-
-    }
-
     public class PaymentService
     {
         public Payment CreateActivePayment(Ticket ticket)
